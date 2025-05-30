2b25b4218ecebc2f9b77430928d2d8cc88e16a13
yuramayer <votetopovorot2015@gmail.com>
2025-05-30 14:15:32 +0300

    feat: added updating each time user goes to the web page

--- script.js
+++ script.js
@@ -10,19 +10,13 @@ window.onload = () => {
     const saved = localStorage.getItem("theme") || "light";
     document.documentElement.setAttribute("data-theme", saved);
 
-    fetch("https://api.open-meteo.com/v1/forecast?latitude=55.75&longitude=37.61&current_weather=true")
-        .then(res => res.json())
-        .then(data => {
-            const code = data.current_weather.weathercode;
-            const { emoji, label } = interpretWeatherCode(code);
-            document.getElementById("emoji").textContent = emoji;
-            document.getElementById("label").textContent = `${label} в Москве`;
-        })
-        .catch(err => {
-            document.getElementById("emoji").textContent = "❌";
-            document.getElementById("label").textContent = "Ошибка загрузки";
-            console.error(err);
-        });
+    fetchWeather();
+
+    document.addEventListener("visibilitychange", () => {
+        if (document.visibilityState === "visible") {
+            fetchWeather();
+        }
+    });
 };
 
 function interpretWeatherCode(code) {
@@ -30,4 +24,31 @@ function interpretWeatherCode(code) {
     if ([2, 3].includes(code)) return { emoji: "☁️", label: "Облачно" };
     if ([51, 61, 63, 65, 80, 81, 82].includes(code)) return { emoji: "🌧️", label: "Дождь" };
     return { emoji: "❓", label: "Неизвестно" };
+}
+
+function fetchWeather() {
+    // показать лоадер
+    document.getElementById("emoji").textContent = "⏳";
+    document.getElementById("label").textContent = "Обновляем погоду...";
+
+    // загружаем погоду
+    fetch("https://api.open-meteo.com/v1/forecast?latitude=55.75&longitude=37.61&current_weather=true")
+        .then(res => res.json())
+        .then(data => {
+            const code = data.current_weather.weathercode;
+            const { emoji, label } = interpretWeatherCode(code);
+
+            // через 5 сек обновим экран
+            setTimeout(() => {
+                document.getElementById("emoji").textContent = emoji;
+                document.getElementById("label").textContent = `${label} в Москве`;
+            }, 1500);
+        })
+        .catch(err => {
+            setTimeout(() => {
+                document.getElementById("emoji").textContent = "❌";
+                document.getElementById("label").textContent = "Ошибка загрузки";
+            }, 1500);
+            console.error(err);
+        });
 }
